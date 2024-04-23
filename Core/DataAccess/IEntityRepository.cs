171b12808e287d20057f86e36c511bf664b1e1c1
zulalkaradepe <zulalkaradepe12@gmail.com>
2024-04-23 19:14:42 +0300

    6 şubat

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,5 +1,5 @@
-﻿using Entities.Abstract;
-using Entities.Concrete;
+﻿//using Entities.Concrete;
+using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +7,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //generic constraint(generik kısıt)
     //class: referans tip olabilir anlamında
